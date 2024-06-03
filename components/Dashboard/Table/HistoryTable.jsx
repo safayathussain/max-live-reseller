0261cf8513a275f88ba96ae989b3cdb99ed6fe88
Safayat <safayatofficial1@gmail.com>
2024-06-03 22:20:27 +0600

    pagination added

--- components/Dashboard/Table/HistoryTable.jsx
+++ components/Dashboard/Table/HistoryTable.jsx
@@ -8,83 +8,29 @@ import { FetchApi } from "@/utils/FetchApi";
 import TextInput from "@/components/TextInput";
 
 export default function HistoryTable() {
-    const dummyusers = [
-        {
-            sl: 1,
-            id: 1,
-            fullName: "Safayat Hussain",
-            email: "safayat@gmail.com",
-            date: "12 May, 2023",
-            beans: 1203,
-        },
-        {
-            sl: 2,
-            id: 1,
-            fullName: "Safayat Hussain",
-            email: "safayat@gmail.com",
-            date: "12 May, 2023",
-            beans: 1203,
-        },
-        {
-            sl: 3,
-            id: 1,
-            fullName: "Safayat Hussain",
-            email: "safayat@gmail.com",
-            date: "12 May, 2023",
-            beans: 1203,
-        },
-        {
-            sl: 4,
-            id: 1,
-            fullName: "Safayat dsa Hussain",
-            email: "safayat@gmail.com",
-            date: "12 May, 2023",
-            beans: 1203,
-        },
-        {
-            sl: 5,
-            id: 1,
-            fullName: "Safayat dsa Hussain",
-            email: "safayat@gmail.com",
-            date: "12 May, 2023",
-            beans: 1203,
-        },
-        {
-            sl: 6,
-            id: 1,
-            fullName: "Safayat dsa Hussain",
-            email: "safayat@gmail.com",
-            date: "12 May, 2023",
-            beans: 1203,
-        },
-
-    ];
+    
     const [users, setUsers] = useState([]);
-    const [paginationData, setPaginationData] = useState({})
     const [searchTerm, setSearchTerm] = useState("");
     const [sortBy, setSortBy] = useState(null);
     const [sortOrder, setSortOrder] = useState("asc");
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage] = useState(5);
     const [open, setOpen] = useState(false);
-
-    useEffect(() => {
-        const loadData = async () => {
-            const { data } = await FetchApi({ url: `user/getAllUser?page=${currentPage}&limit=5` })
-            setUsers(data?.users?.users)
-            setPaginationData(data?.users?.pagination)
-        }
-        loadData()
-    }, [currentPage])
+    
     const ref = useRef(null);
     useClickOutside(ref, () => {
         setOpen(false);
     });
-
-    // Function to handle search input
-    const handleSearchChange = (e) => {
-        setSearchTerm(e.target.value);
-    };
+    const loadData = async () => {
+        const { data } = await FetchApi({ url: `user/getAllUser` })
+        setUsers(data?.users.users)
+    }
+    useEffect(() => {
+        loadData()
+    }, [])
+    let searchedUsers = users.filter((user) =>
+        user._id.includes(searchTerm)
+    );
 
     // Function to handle sorting
     const handleSort = (key) => {
@@ -98,12 +44,8 @@ export default function HistoryTable() {
         }
     };
 
-    let filteredusers = users.filter((user) =>
-        user?._id?.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
     if (sortBy) {
-        filteredusers = filteredusers.sort((a, b) => {
+        searchedUsers = searchedUsers.sort((a, b) => {
             const compareA = a[sortBy];
             const compareB = b[sortBy];
             if (compareA < compareB) {
@@ -113,13 +55,19 @@ export default function HistoryTable() {
                 return sortOrder === "asc" ? 1 : -1;
             }
             return 0;
-        });
+        })
     }
 
     // Pagination
-    const showingText = `Showing ${(currentPage - 1) * itemsPerPage + 1} to ${(currentPage * itemsPerPage) < paginationData?.totalCount ? (currentPage * itemsPerPage) : paginationData?.totalCount} of ${paginationData?.totalCount}`;
-
+    const showingText = `Showing ${(currentPage - 1) * itemsPerPage + 1} to ${((currentPage - 1) * itemsPerPage) < Math.ceil(searchedUsers.length / itemsPerPage) ? (currentPage * itemsPerPage) : searchedUsers.length} of ${searchedUsers.length}`;
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentUsers = searchedUsers.slice(
+        indexOfFirstItem,
+        indexOfLastItem
+    );
 
     return (
         <div className=" overflow-hidden">
@@ -127,8 +75,8 @@ export default function HistoryTable() {
                 <div className="w-full md:w-1/2 py-1">
                     <form className="flex items-center">
                         <div className="relative w-full">
-                        <TextInput value={searchTerm}
-                                onChange={handleSearchChange} label={'Search By Id'} name={'Search'} id={'idSearch'} />
+                        <TextInput onChange={(e) => setSearchTerm(e.target.value)} placeholder={'Search By Id'} name={'Search'} id={'idSearch'} />
+
                         </div>
                     </form>
                 </div>
@@ -253,7 +201,7 @@ export default function HistoryTable() {
                         </tr>
                     </thead>
                     <tbody>
-                        {filteredusers.map((user, i) => (
+                        {currentUsers.map((user, i) => (
                             <tr key={user._id} className="border-b whitespace-nowrap">
                                 <td className="px-4 py-4">{i + 1}</td>
                                 <td onClick={() => setOpen(true)} className="px-4 py-4 font-medium text-gray-900 whitespace-nowrap cursor-pointer">
@@ -304,10 +252,10 @@ export default function HistoryTable() {
                                     {currentPage}
                                 </button>
                                 {
-                                    currentPage !== Number(paginationData?.totalPages) &&
+                                    currentPage !== Math.ceil(searchedUsers.length / itemsPerPage) &&
                                     <button
                                         disabled={
-                                            currentPage === Number(paginationData?.totalPages)
+                                            currentPage === Math.ceil(searchedUsers.length / itemsPerPage)
                                         }
                                         onClick={() => paginate(currentPage + 1)}
                                         className={`py-2 px-4  bg-white text-gray-700 text-xs sm:text-sm hover:!bg-gray-50 focus:outline-none `}
@@ -323,13 +271,13 @@ export default function HistoryTable() {
                                 <button
                                     className={`py-2 px-4  bg-white text-gray-700 text-xs sm:text-sm hover:bg-gray-100 focus:outline-none `}
                                 >
-                                    {Number(paginationData?.totalPages)}
+                                    {Math.ceil(searchedUsers.length / itemsPerPage)}
                                 </button>
                                 <button
                                     className="py-2 px-4 text-gray-700 bg-gray-100 text-xs sm:text-sm focus:outline-none"
                                     onClick={() => paginate(currentPage + 1)}
                                     disabled={
-                                        currentPage === Number(paginationData?.totalPages)
+                                        currentPage === Math.ceil(searchedUsers.length / itemsPerPage)
                                     }
                                 >
                                     &#x2192;
